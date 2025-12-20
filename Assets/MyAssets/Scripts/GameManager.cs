c0e3ebeb9503d2fc15041fe8131e3adbacac8aa7
12AlexanderV <99037353+12AlexanderV@users.noreply.github.com>
2025-12-20 18:36:37 -0500

    CinemachineCamera

--- Assets/MyAssets/Scripts/GameManager.cs
+++ Assets/MyAssets/Scripts/GameManager.cs
@@ -1,10 +1,46 @@
 using System;
+using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PlayerController[] players;
 
+    private List<PlayerInput> playersCamera = new List<PlayerInput>();
+    [SerializeField]
+    private List<Transform> startingPoints;
+    [SerializeField]
+    private List<LayerMask> playerLayers;
+
+    private PlayerInputManager playerInputManager;
+
+    private void Awake()
+    {
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
+    }
+
+
+    public void AddPlayer(PlayerInput player)
+    {
+        playersCamera.Add(player);
+
+        //need to use the parent due to the structure of the prefab
+        Transform playerParent = player.transform.parent;
+        playerParent.position = startingPoints[playersCamera.Count - 1].position;
+
+        //convert layer mask (bit) to an integer 
+        int layerToAdd = (int)Mathf.Log(playerLayers[playersCamera.Count - 1].value, 2);
+
+        //set the layer
+        playerParent.GetComponentInChildren<CinemachineCamera>().gameObject.layer = layerToAdd;
+        //add the layer
+        playerParent.GetComponentInChildren<Camera>().cullingMask |= 1 << layerToAdd;
+        //set the action in the custom cinemachine Input Handler
+        //playerParent.GetComponentInChildren<InputHandler>().horizontal = player.actions.FindAction("Look");
+
+    }
     private void OnEnable()
     {
         PlayerController.OnPlayerDied += HandlePlayerDied;
